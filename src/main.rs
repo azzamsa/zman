c978454e8521026c008af0b656895d96714319dc
azzamsa <me@azzamsa.com>
2020-09-28 09:57:25 +0700

    feat: add CLI support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,50 @@
 mod progress;
 
+use clap::{crate_version, App, AppSettings, Arg};
+
 fn main() {
-    show_year_progress()
+    let mut _is_json: bool = false;
+    let matches = App::new("Jaro [A CLI time progress bar]")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .setting(AppSettings::ColoredHelp)
+        .version(crate_version!())
+        .arg(
+            Arg::new("year")
+                .short('y')
+                .long("year")
+                .about("Show current year progress"),
+        )
+        .arg(
+            Arg::new("month")
+                .short('m')
+                .long("month")
+                .about("Show current month progress"),
+        )
+        .arg(
+            Arg::new("week")
+                .short('w')
+                .long("wek")
+                .about("Show current week progress"),
+        )
+        .arg(
+            Arg::new("json")
+                .short('j')
+                .long("json")
+                .about("Display progress in JSON formatted string"),
+        )
+        .get_matches();
+
+    if matches.is_present("json") {
+        _is_json = true;
+    } else {
+        _is_json = false;
+    }
+
+    if matches.is_present("year") {
+        show_year_progress();
+    }
 }
+
 
 fn show_year_progress() {
     progress::show_progress(20)
