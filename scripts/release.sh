d4b23005a8a4aea6ddc3e186c409f9331b836ee7
azzamsa <me@azzamsa.com>
2020-09-29 20:07:04 +0700

    build: add warning to update related release file

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,8 @@
 VERSION=`awk '/version = "[0-9]\.[0-9]\.[0-9]"/' Cargo.toml | awk -F\" '{print $2}'`
 
+# init
+cargo fmt
+
 # GNU/Linux
 cargo build --release
 strip target/release/zman
@@ -8,3 +11,7 @@ mv target/release/zman target/release/zman-$VERSION
 # Windows PC
 cargo build --release --target=x86_64-pc-windows-gnu
 mv target/x86_64-pc-windows-gnu/release/zman.exe target/x86_64-pc-windows-gnu/release/zman-$VERSION.exe
+
+# don't forget!
+echo "Check the changelog!" >> CHANGELOG.md
+echo "Check the version!" >> Cargo.toml
