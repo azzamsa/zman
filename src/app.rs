c85e9cdbbb92b0669851233884115114726fe38f
azzamsa <me@azzamsa.com>
2022-05-18 15:04:16 +0700

    Revert "refactor: port to clap v2"
    This reverts commit 4ac07944893403d4a417077d49d77971235b3de8.

--- src/app.rs
+++ src/app.rs
@@ -1,46 +1,46 @@
 use clap::{crate_version, App, AppSettings, Arg};
 
 /// Build a clap app
-pub fn build() -> App<'static, 'static> {
+pub fn build() -> App<'static> {
     let app = App::new("Zman [A CLI time progress bar]")
         .setting(AppSettings::ArgRequiredElseHelp)
         .setting(AppSettings::ColoredHelp)
         .version(crate_version!())
         .arg(
-            Arg::with_name("time")
+            Arg::new("time")
                 .possible_values(&["year", "month", "week"])
                 .hide_possible_values(true)
                 .default_value("year")
                 .takes_value(true)
-                .help("A time to show"),
+                .about("A time to show"),
         )
         .arg(
-            Arg::with_name("json")
-                .short("J")
+            Arg::new("json")
+                .short('J')
                 .long("json")
-                .help("Display progress in JSON formatted string"),
+                .about("Display progress in JSON formatted string"),
         )
         .arg(
-            Arg::with_name("width")
-                .short("d")
+            Arg::new("width")
+                .short('d')
                 .long("width")
-                .help("Adjust width of the bar (default: 20)")
+                .about("Adjust width of the bar (default: 20)")
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("full_bar")
-                .short("f")
+            Arg::new("full_bar")
+                .short('f')
                 .long("full-bar")
                 .default_value("\u{2593}")
-                .help("Set full bar icon")
+                .about("Set full bar icon")
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("rest_bar")
-                .short("r")
+            Arg::new("rest_bar")
+                .short('r')
                 .long("rest-bar")
                 .default_value("\u{2591}")
-                .help("Set rest bar icon")
+                .about("Set rest bar icon")
                 .takes_value(true),
         );
     app
