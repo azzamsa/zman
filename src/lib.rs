4154042f6f97418164d817bc8dc45578675f0e0f
azzamsa <me@azzamsa.com>
2022-12-17 18:22:11 +0700

    refactor: remove unused `clippy` rules

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-#![allow(
-    clippy::missing_const_for_fn,
-    clippy::must_use_candidate,
-    clippy::cast_possible_truncation,
-    clippy::cast_precision_loss,
-    clippy::cast_sign_loss
-)]
-
 pub mod cli;
 pub mod output;
 pub mod progress;
