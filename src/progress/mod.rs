43fad2c1bbde873c94e53222c0808df85114a2af
Azzam S.A <noreply@azzamsa.com>
2025-06-10 20:11:08 +0700

    build: upgrade `jiff` from v0.1 to v0.2
    It is safe to assume all days are 24 hours.
    As this tool just to show the progress bar.

--- src/progress/mod.rs
+++ src/progress/mod.rs
@@ -5,21 +5,23 @@ pub use month::month;
 pub mod week;
 pub use week::week;
 
-use jiff::{Unit, civil};
+use jiff::{Span, SpanTotal, Unit, civil};
 
 use crate::error::Error;
 
 // Calculate the ratio of time progress
 fn compute(current: civil::Date, start: civil::Date, end: civil::Date) -> Result<f64, Error> {
-    let whole_diff = end - start;
+    let whole_diff: Span = end - start;
     // 86_400 is total second in a day (24 * 3600)
     let total_seconds_in_day = 84_000.0;
-    let whole_diff_in_seconds = whole_diff.total(Unit::Day)? * total_seconds_in_day
-        + whole_diff.total(Unit::Millisecond)?;
+    let whole_diff_in_seconds = whole_diff.total(SpanTotal::from(Unit::Day).days_are_24_hours())?
+        * total_seconds_in_day
+        + whole_diff.total(SpanTotal::from(Unit::Millisecond).days_are_24_hours())?;
 
     let current_diff = current - start;
-    let current_diff_in_seconds = current_diff.total(Unit::Day)? * total_seconds_in_day
-        + current_diff.total(Unit::Millisecond)?;
+    let current_diff_in_seconds =
+        current_diff.total(SpanTotal::from(Unit::Day).days_are_24_hours())? * total_seconds_in_day
+            + current_diff.total(SpanTotal::from(Unit::Millisecond).days_are_24_hours())?;
     // progress_ratio
     let ratio = current_diff_in_seconds / whole_diff_in_seconds;
     Ok(ratio)
